fc823c407b6386d412aa08f349539cd86b4215f5
Soo Hyeok Lee <39535216+soohyeok@users.noreply.github.com>
2018-06-04 00:20:12 -0400

    Add files via upload

---
+++ HW1/app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index'); //routes/index.js
+var usersRouter = require('./routes/users'); //routes/users.js
+var hw1Router = require('./routes/hw1');
+
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/hw1', hw1Router)
+
+
+router.get('/admin', function (req, res, next) { ... })
+
+It is mounted with
+
+    const admin = require('../routes/admin')
+app.use('/user', admin)
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ HW1/routes/hw1.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended:true}));
+
+
+
+// router.get('/', function(req, res, next) {
+//     res.render('index', { title: 'Express' });
+// });
+
+router.post('/', function(req, res, next) {
+    var stringName = req.body.stringName;
+    var stringLength = req.body.stringLength;
+    res.send('{"String":"' + stringName + '", "length":' + stringLength + '}');
+});
+
+//{"String":"hello","length":5}
+
+
+router.get('/:String', function(req, res, next) {
+    (req.params).length =(req.params.String).length
+    res.json(req.params);
+});
+
+module.exports = router;
