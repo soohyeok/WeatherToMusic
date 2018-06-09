1e65546972ba9b41f5da026a86fd4586ed0826f8
Soo Hyeok Lee <39535216+soohyeok@users.noreply.github.com>
2018-06-09 00:53:53 -0400

    Update hw1.js

--- HW2/routes/hw1.js
+++ HW2/routes/hw1.js
@@ -1,16 +1,28 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
 
-/* GET home page. */
-router.get('/:string', function(req, res, next) {
-     (req.params).length = ((req.params).string).length;
-     res.json(req.params);
-});
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended:true}));
+
+
+
+// router.get('/', function(req, res, next) {
+//     res.render('index', { title: 'Express' });
+// });
 
 router.post('/', function(req, res, next) {
-    let string = req.body.string;
-    let length = string.length;
-    res.send('{"string":' + '"' + string + '", "length":' + length + '}');
-})
+    var string = (req.body).string;
+    var stringLength = string.length;
+    res.send('{"string":"' + string + '", "length":' + stringLength + '}');
+});
+
+//{"String":"hello","length":5}
+
+
+router.get('/:string', function(req, res, next) {
+    (req.params).length =((req.params).string).length
+    res.json(req.params);
+});
 
 module.exports = router;
