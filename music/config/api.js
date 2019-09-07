f1e389ecad42b8e5ea35354a9320b72f4ec5db15
Soo Hyeok Lee <39535216+soohyeok@users.noreply.github.com>
2019-09-06 20:35:10 -0400

    Update api.js

--- music/config/api.js
+++ music/config/api.js
@@ -1,10 +1,10 @@
 const api = {
 	weather: {
-		key: '0bc76ae3e90954040cd248860bc9c6e8'
+		key: '080bc76ae3e90954040cd248860bc9c6e822'
 	},
 	spotify:{
-		client_id: 'f903ee52b206433e83b6e676adf92759',
-		client_secret: '4cca42fee3fb45d287116b80585a6105'
+		client_id: '08f903ee52b206433e83b6e676adf9275922',
+		client_secret: '084cca42fee3fb45d287116b80585a610522'
 	}
 }
 
